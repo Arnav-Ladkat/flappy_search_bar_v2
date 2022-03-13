b4b17539feb4d5db1f8cfc14341058bbabedaacb
arnav-ladkat <arnav@hlth.dev>
2022-03-14 03:07:24 +0530

    initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,137 @@
+import 'dart:math';
+
+import 'package:flappy_search_bar_v2/flappy_search_bar_v2.dart';
+import 'package:flappy_search_bar_v2/scaled_tile.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Post {
+  final String title;
+  final String body;
+
+  Post(this.title, this.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final SearchBarController<Post> _searchBarController = SearchBarController();
+  bool isReplay = false;
+
+  Future<List<Post>> _getALlPosts(String? text) async {
+    await Future.delayed(Duration(seconds: text?.length == 4 ? 10 : 1));
+    if (isReplay) return [Post("Replaying !", "Replaying body")];
+    if (text?.length == 5) throw Error();
+    if (text?.length == 6) return [];
+    List<Post> posts = [];
+
+    var random = Random();
+    for (int i = 0; i < 10; i++) {
+      posts
+          .add(Post("$text $i", "body random number : ${random.nextInt(100)}"));
+    }
+    return posts;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SearchBar<Post>(
+          searchBarPadding: const EdgeInsets.symmetric(horizontal: 10),
+          headerPadding: const EdgeInsets.symmetric(horizontal: 10),
+          listPadding: const EdgeInsets.symmetric(horizontal: 10),
+          onSearch: _getALlPosts,
+          searchBarController: _searchBarController,
+          placeHolder: const Text("placeholder"),
+          cancellationWidget: const Text("Cancel"),
+          emptyWidget: const Text("empty"),
+          indexedScaledTileBuilder: (int index) =>
+              ScaledTile.count(1, index.isEven ? 2 : 1),
+          header: Row(
+            children: <Widget>[
+              ElevatedButton(
+                child: const Text("sort"),
+                onPressed: () {
+                  _searchBarController.sortList((Post a, Post b) {
+                    return a.body.compareTo(b.body);
+                  });
+                },
+              ),
+              ElevatedButton(
+                child: Text("Desort"),
+                onPressed: () {
+                  _searchBarController.removeSort();
+                },
+              ),
+              ElevatedButton(
+                child: Text("Replay"),
+                onPressed: () {
+                  isReplay = !isReplay;
+                  _searchBarController.replayLastSearch();
+                },
+              ),
+            ],
+          ),
+          onCancelled: () {
+            debugPrint("Cancelled triggered");
+          },
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          crossAxisCount: 2,
+          onItemFound: (Post post, int index) {
+            return Container(
+              color: Colors.lightBlue,
+              child: ListTile(
+                title: Text(post.title),
+                isThreeLine: true,
+                subtitle: Text(post.body),
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Detail()));
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  const Detail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            const Text("Detail"),
+          ],
+        ),
+      ),
+    );
+  }
+}
